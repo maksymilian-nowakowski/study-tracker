ffb64f931ab4e1b518b8b8f00a61b95a7cc10d55
Maksymilian Nowakowski <maks.nowakowski.pro@gmail.com>
2026-01-11 12:52:20 +0000

    feat: read stored data

--- study_tracker.py
+++ study_tracker.py
@@ -1,3 +1,5 @@
+sessions = []
+
 date = input("Enter date (YYYY-MM-DD):  ")
 hours = input("Hours studied today: ")
 
@@ -5,3 +7,10 @@ with open("study_data.txt", "a") as file:
     file.write(f"{date},{hours}\n")
 
 print("Study session saved.")
+
+with open("study_data.txt", "r") as file:
+    for line in file:
+        date, hours = line.strip().split(",")
+        sessions.append((date, float(hours)))
+
+print(sessions)
